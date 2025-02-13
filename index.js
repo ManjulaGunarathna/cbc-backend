db3d44682793a062a879b879c1918f0643658baa
manjulagunarathna <manjulagunarathna500@gmail.com>
2025-02-13 22:44:37 +0530

    Async await creation and env config

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ import mongoose, { get } from 'mongoose';
 import productRouter from './routes/productRouter.js';
 import userRouter from './routes/userRouter.js';
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config()
 
 const app = express();
 
-const mongoUrl = "mongodb+srv://admin:123@cluster0.mu6bn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const mongoUrl = process.env.MONGO_DB_URI
 mongoose.connect(mongoUrl,{})
 
 const connection = mongoose.connection;
