2f02a6d235c0b803a7372ffa9980b13acdeb7c9c
manjulagunarathna <manjulagunarathna500@gmail.com>
2025-02-16 17:11:30 +0530

    Restrict create admin accounts

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,6 +8,21 @@ export function createUser(req,res){
 
     const newUserData = req.body
 
+    if(newUserData.type == "admin"){
+        if(req.user==null){
+            res.json({
+                message: "Please login as administrator to create admin accounts"
+            })
+            return
+        }
+        if(req.user.type != "admin"){
+            res.json({
+                message: "Please login as administrator to create admin accounts"
+            })
+            return
+        }
+    }
+
     newUserData.password = bcrypt.hashSync(newUserData.password, 10)
 
     const user = new User(newUserData)
@@ -59,3 +74,4 @@ export function loginUser(req,res){
         }
     )
 }
+// manjula.doe@example.com            securepassword123
