d63b7af01f3ff70e076dc56fd642fa21728c5a61
manjulagunarathna <manjulagunarathna500@gmail.com>
2025-02-16 19:36:34 +0530

    Create productRouter.js

---
+++ routes/productRouter.js
@@ -0,0 +1,6 @@
+import express from "express";
+import { createProduct } from "../controllers/productController.js";
+
+const productRouter = express.Router();
+
+productRouter.post("/",createProduct)
